350068075d64d18f639045b2f3d06a092e19e82b
Michael Quigley <mikequigley@quigleypublishing.com>
2019-12-30 01:19:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   squared_array = []
     while array[counter] do
       squared_array <<(array[counter]**2)
-      counter += 1
+      counter += 11
     end
   return squared_array
 end
