31a6fc1d904915e99d8e01ceca31d98778cf2044
Michael Quigley <mikequigley@quigleypublishing.com>
2019-12-30 01:48:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,19 @@
-def square_array(array)
-  counter = 0
+#def square_array(array)
+#  counter = 0
+#  squared_array = []
+#    while array[counter] do
+#      squared_array <<(array[counter]**2)
+#      counter += 1
+#    end
+#  return squared_array
+#end
+
+
+#test out higher-level iterators? (each, collect, map, inject)
+def square_array_with_each(array)
   squared_array = []
-    while array[counter] do
-      squared_array <<(array[counter]**2)
-      counter += 1
-    end
-  return squared_array
+  array.each {|x| squared_array << (x**2)}
+  squared_array
 end
 
 
