73b1dc7b630002088fb8a76c1030d9a508d53f96
Michael Quigley <mikequigley@quigleypublishing.com>
2019-12-30 00:57:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,23 @@
 def square_array(array)
-  # your code here
+  counter = 0 
+  squared_array = []
+    while array[counter] do
+      squared_array <<(array[counter]**2)
+      counter += 1
+    end
+  return squared_array
 end
+
+
+
+#Practice Example based on the example in "Looping with arrays"
+#def ar (pets)
+#  counter = 0
+#  new_pets = []
+#    while pets[counter] do
+#    #puts (pets[counter]**2)
+#    new_pets << (pets[counter]**2)
+#    counter += 1
+#    end
+#  return new_pets
+#end
